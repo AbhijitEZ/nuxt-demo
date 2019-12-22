85a60545b575f7a3b3e82919e5aec8dbc04b1645
abhijitez <abhijit.nair659@gmail.com>
2019-12-22 14:25:56 +0530

    feat: 🎸 First Commit
    Nuxtjs SPA project with buefy

---
+++ components/Carousel.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-carousel>
+        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+            <section :class="`hero is-medium is-${carousel.color}`">
+                <div class="hero-body has-text-centered">
+                    <h1 class="title">{{carousel.text}}</h1>
+                </div>
+            </section>
+        </b-carousel-item>           
+    </b-carousel>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            carousels: [
+                { text: 'Nuxtjs', color: 'primary' },
+                { text: 'Vue', color: 'info' },
+                { text: 'Buefy', color: 'success' },
+                { text: 'Vuex', color: 'warning' },
+                { text: 'Vue-Router', color: 'danger' }
+            ]
+        }
+    }
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <div class="column">
+      <carousel />
+    </div>
+  </section>
+</template>
+<script>
+import Carousel from '~/components/Carousel'
+
+export default {
+  name: 'Inspire',
+
+  components: {
+    Carousel
+  }
+}
+</script>
